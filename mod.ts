3d234506996b7922089a5d9a20af4de5fe001332
Jack McCown <jackmac79@gmail.com>
2021-08-24 13:33:56 -0400

    updoots

--- mod.ts
+++ mod.ts
@@ -1,11 +1,24 @@
+import * as path from "https://deno.land/std/path/mod.ts";
 import createLogger from "./logger.ts";
 
 const homeDir = Deno.env.get("HOME");
 if (homeDir === undefined) {
-  throw new Error("Can't find home dir, why don't you have $HOME set?")
+  throw new Error("Can't find home dir, why don't you have $HOME set?");
 }
 
 const logger = await createLogger(`${homeDir}/.local/hackydanger.log`);
+
+const decoder = new TextDecoder();
+
+const cmdResponse = async (...cmd: string[]) => {
+  const p = Deno.run({ cmd, stdin: "piped", stdout: "piped" });
+
+  await p.status();
+
+  const o = await p.output();
+  const out = decoder.decode(o);
+  return out;
+};
 
 logger.info("Incoming");
 
@@ -20,12 +33,12 @@ try {
 const action = req.host;
 
 const subAction = req.pathname;
-const params_AnnoyingFormat = new URLSearchParams(req.search);
+const paramsAnnoyingFormat = new URLSearchParams(req.search);
 
 const params: { [k: string]: unknown } = {};
 
-for (const p of params_AnnoyingFormat.keys()) {
-  params[p] = params_AnnoyingFormat.get(p);
+for (const p of paramsAnnoyingFormat.keys()) {
+  params[p] = paramsAnnoyingFormat.get(p);
 }
 
 const defaultCmdOpts: { env?: { [key: string]: string }; cwd: string } = {
@@ -33,7 +46,7 @@ const defaultCmdOpts: { env?: { [key: string]: string }; cwd: string } = {
   env: {},
 };
 
-const runCmdInTmux = (cmd: string, options = defaultCmdOpts) => {
+const _runCmdInTmux = (cmd: string, options = defaultCmdOpts) => {
   // accept params
   const x = Deno.run({
     ...options,
@@ -62,15 +75,15 @@ logger.info(`Handling ${action}`);
 switch (action) {
   case "repoactivate":
     throw new Error(`unimplemented!`);
-    break;
-  case "popupexec":
+  case "popupexec": {
     const targetExecStr = params.script;
     if (typeof targetExecStr !== "string") {
       throw new Error("popupexec received non string target cmd");
     }
     await runCmdInPopupShell(`${targetExecStr}`);
     break;
-  case "sscript":
+  }
+  case "sscript": {
     const targetCmd = params.script;
     if (typeof targetCmd !== "string") {
       throw new Error("sscript received non string target cmd");
@@ -79,7 +92,8 @@ switch (action) {
       `/home/jmccown/.config/custom/path_scripts/s ${targetCmd}`
     );
     break;
-  case "youtubedl":
+  }
+  case "youtubedl": {
     const targetUrl = params.url;
     if (typeof targetUrl !== "string") {
       throw new Error("youtube-dl received non string target url");
@@ -88,11 +102,43 @@ switch (action) {
       cwd: `${homeDir}/Downloads`,
     });
     break;
+  }
   case "cbissh":
     throw new Error(`unimplemented!`);
+  case "localDev": {
+    const { fileUrl } = params;
+    if (typeof fileUrl !== "string") {
+      throw new Error("Invalid file url");
+    }
+    const url = new URL(fileUrl);
+    const lineNo = "0";
+    const repoName = url.pathname.split("/")[2];
+    const inRepofilePath = url.pathname.slice(3);
+    const repoDir = await cmdResponse(
+      "/home/jmccown/.nix-profile/bin/zoxide",
+      "query",
+      repoName
+    );
+    const filePath = path.join(repoDir, inRepofilePath);
+    // TODO how to make this command switch projectile project, to activate the workspace and reopen existing
+    logger.debug(`opening ${repoName} at ${repoDir}`);
+    // const cmd = [
+    //   "/home/jmccown/.nix-profile/bin/emacsclient",
+    //   `+${lineNo} `,
+    //   filePath,
+    // ];
+    const cmd = [
+      "/home/jmccown/.nix-profile/bin/emacsclient",
+      "-e",
+      `(counsel-projectile-switch-project-by-name ${repoDir})`,
+    ];
+    const p = Deno.run({ cmd, stdout: "piped", stderr: "piped" });
+    await p.status();
     break;
+  }
   case "namedscript": {
-    Deno.run({ cmd: [subAction], stdout: "piped", stderr: "piped" });
+    const p = Deno.run({ cmd: [subAction], stdout: "piped", stderr: "piped" });
+    await p.status();
     break;
   }
   default:
