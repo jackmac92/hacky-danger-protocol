61758615a9821d4ec93993b4d75a53229f8a6342
Jack McCown <jackmac79@gmail.com>
2021-10-25 17:06:40 -0400

    update xdotool helpers

--- xdotool.ts
+++ xdotool.ts
@@ -1,16 +1,18 @@
-import { cmdResponse } from "./mod";
+import { cmdResponse } from "./cmdResponse.ts";
 
-const xdotool = (...cmd: string[]) => cmdResponse("xdotool", ...cmd);
-const xdotoolCurrentWindow = () => xdotool("getactivewindow");
+export const xdotool = (...cmd: string[]) => cmdResponse("xdotool", ...cmd);
+
+export const xdotoolCurrentWindow = () => xdotool("getactivewindow");
+
 export const xdotoolOpenActive = async (appName: string) => {
-    const existingWinId = await xdotool(
-        "search",
-        "--onlyvisible",
-        "--name",
-        appName
-    );
-    if (!existingWinId) {
-        throw new Error("No such window found");
-    }
-    await xdotool("windowfocus", "--sync", existingWinId);
+  const existingWinId = await xdotool(
+    "search",
+    "--onlyvisible",
+    "--name",
+    appName
+  );
+  if (!existingWinId) {
+    throw new Error("No such window found");
+  }
+  await xdotool("windowfocus", "--sync", existingWinId);
 };
